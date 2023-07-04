f512ce1ddd13d63f3597b4e38fed8e6942bd67d3
배대철 <qoeocjf5@gmail.com>
2023-07-04 23:19:33 +0900

    Add: LV2_게임맵최단거리

---
+++ LV2_게임맵최단거리.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+def solution(maps):
+        #상하좌우
+    dx=[-1,1,0,0]
+    dy=[0,0,-1,1]
+    n=len(maps)
+    m=len(maps[0])
+    
+    def bfs(x,y):
+        queue=deque()
+        queue.append((x,y))    
+
+        #큐가 빌때까지
+        while queue:
+            x,y=queue.popleft()
+
+            #상하좌우 보기
+            for i in range(4):
+                nx=x+dx[i] #새로운 x	
+                ny=y+dy[i] #새로운 y
+
+                #예외 케이스: 범위를 벗어나는가 
+                if nx <0 or ny <0 or nx >=n or ny >=m:
+                    continue
+                #벽인가
+                if maps[nx][ny]==0:
+                    continue
+                #방문하지 않았으면 최단거리 기록 
+                if maps[nx][ny]==1:
+                    maps[nx][ny]=maps[x][y]+1
+                    queue.append((nx,ny))
+
+        return maps[n-1][m-1]
+
+    answer=bfs(0,0)
+    if answer==1:
+        return -1
+    
+    return answer
+
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+# print(solution([[1, 0, 1], [1, 1, 1], [1, 0, 1] ]))
