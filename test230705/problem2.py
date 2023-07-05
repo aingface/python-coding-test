8bf1782378c7b7df9ee5c60a2260b11025e40051
배대철 <qoeocjf5@gmail.com>
2023-07-05 17:59:02 +0900

    Add: 임시 230705 문제

---
+++ test230705/problem2.py
@@ -0,0 +1,13 @@
+def solution(blocks, k):
+    answer=0
+
+
+    return answer
+
+# 예시 실행
+blocks1 = [1, 2, 3, 2, 4, 2]
+blocks2 = [1, 2, 1, 1]
+blocks3 = [1, 1, 1]
+k = 2
+
+print(solution(blocks1, k))  # 출력: 5
---
+++ test230705/problem3.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+def solution(card1, card2):
+    answer=0
+
+    return answer
+
+# 예시 실행
+card11 = [1, 1, 2, 3,3,3]
+card12 = [1,1,2,3,3,4]
+
+card21 = [1, 1, 2, 2,3,3,4]
+card22 = [1,1,2,2,2,3,3]
+
+card31 = [7,7,7,7,8,8,8]
+card32 = [4,7,8,12,12,12,12]
+
+card41=[1,1,1,1,1,1]
+card42=[2,1,1,2,3,5]
+
+
+result = solution(card41, card42)
+print(result)
